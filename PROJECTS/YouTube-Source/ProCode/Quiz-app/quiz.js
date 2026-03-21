73c3fd0bf525701f605ae84ce652c0a84915d4fc
parmeshwar-surwase <parmeshwar.surwase.dev@gmail.com>
2026-03-21 19:33:26 +0530

    Quiz App

--- PROJECTS/YouTube-Source/ProCode/Quiz-app/quiz.js
+++ PROJECTS/YouTube-Source/ProCode/Quiz-app/quiz.js
@@ -72,5 +72,27 @@ console.log("answer element", answerElement);
 
 
 submit.addEventListener("click", () => {
-    
+    const checkedAns = document.querySelector('input[type="radio"]:checked');
+    console.log(checkedAns);
+    // console.log(checkedAns.nextElementSibling.textContent);
+    if (checkedAns === null) {
+        alert("Please select an answer");
+    } else {
+        if (checkedAns.nextElementSibling.textContent === quiz[currentQuestion].answer) {
+            score++;
+        }
+
+        currentQuestion++;
+        if (currentQuestion < quiz.length) {
+            question.textContent = quiz[currentQuestion].question;
+            option_a.textContent = quiz[currentQuestion].ans1text
+            option_b.textContent = quiz[currentQuestion].ans2text
+            option_c.textContent = quiz[currentQuestion].ans3text
+            option_d.textContent = quiz[currentQuestion].ans4text
+            checkedAns.checked = false;
+        }else {
+            alert("Your score is " + score + " out of " +quiz.length);
+            location.reload();
+        }
+    }
 });
