d9e98ee17a12e81049013486b3ec022c43a389e5
parmeshwar-surwase <parmeshwar.surwase.dev@gmail.com>
2026-03-28 10:18:39 +0530

    JS completed

--- PROJECTS/YouTube-Source/ProCode/_05_Password-Generator/password.js
+++ PROJECTS/YouTube-Source/ProCode/_05_Password-Generator/password.js
@@ -3,8 +3,8 @@ const upper = document.querySelector('.uppercase');
 const lower = document.querySelector('.lowercase');
 const number = document.querySelector('.numbers');
 const symbol = document.querySelector('.symbols');
-const passinp = document.querySelector('.pass-input');
-const copy = document.querySelector('.copy');
+const passinp = document.querySelector('#pass-input');
+const copy = document.querySelector('#copy');
 const generate = document.querySelector('#generate');
 
 console.log(lengthp.value);
@@ -18,6 +18,7 @@ const lowercasestr = 'abcdefghijklmnopqrstuvwxyz';
 const numberstr = '0123456789';
 const symbolstr = '!@#$%^&*()_+';
 
+let password = '';
 generate.addEventListener('click', () => {
 
     let str = '';
@@ -39,5 +40,32 @@ generate.addEventListener('click', () => {
     }
 
     console.log(str,"string");
+    
+    for (let i=0; i<lengthp.value; i++){
+        console.log(str.length, "str.length");
+        let index = Math.floor(Math.random()*str.length);
+        console.log(index, "index");
+        console.log("Math.random()",Math.random());
+        console.log("Math.random()*str.length", Math.random()*str.length);
+        console.log("Math.floor(Math.random()*str.lenght)", Math.floor(Math.random()*str.length));
+        console.log(str[index]);
+        password += str[index];
+    }
+    console.log(password,"password");
+    passinp.value = password;
 
+});
+
+copy.addEventListener('click', () => {
+    if(passinp.value === ''){
+        alert('Please Generate a Password First');
+    }else {
+        const newele = document.createElement('textarea');
+        newele.value = passinp.value;
+        document.body.appendChild(newele);
+        newele.select();
+        document.execCommand('copy');
+        alert('Password Copied to Clipboard');
+        newele.remove();
+    }
 });
